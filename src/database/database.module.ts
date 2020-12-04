31b685dcc7258f7cb6939d2c922c5bb4af109b74
felixpotie <felixpotie@gmail.com>
2020-12-04 15:32:02 +0100

    env variable

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../model/user.entity';
+import { configService } from '../config.service';
 
 @Module({
-    imports: [TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'bilan_carbone_db',
-        entities: [User],
-        synchronize: true,
-    })]
+    imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())]
 })
 export class DatabaseModule {}
