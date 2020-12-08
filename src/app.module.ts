7f2b4bf4d716b804001cd6d4893a2519b951a894
Mathis <mathis.bourrat@etu.umontpellier.fr>
2020-12-08 15:20:06 +0100

    implement mail-sender

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { UsersModule } from './module/users.module';
 import { DatabaseModule } from './module/database.module';
+import { MailModule } from './module/mail.module';
 
 @Module({
-    imports: [UsersModule, DatabaseModule],
+    imports: [
+        UsersModule, 
+        DatabaseModule,
+        MailModule
+    ],
     providers: [AppService],
     controllers: [AppController],
 })
