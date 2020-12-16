0b2be3b24eea20c972607a2b1e9b30862d9ea5fa
Mathis <mathis.bourrat@etu.umontpellier.fr>
2020-12-16 13:58:37 +0100

    modify infos in user's access token

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -27,14 +27,25 @@ export class AuthService {
             username: username,
             password: password
         });
-        if(bind.success === true )
-            return {
-                username: username
-            };
+        if (bind.success === true) {
+            const search: any = await this.handleSearch({
+                username: username,
+                password: password
+            })
+            const user = search.user
+            if (user) {
+                return {
+                    username: username,
+                    department: user.department,
+                    mail: user.mail,
+                    departmentNumber: user.departmentNumber
+                };
+            }
+        }
         return null;
     }
 
-    private handleBind(req: any){
+    private handleBind(req: any) {
         let ldapClient = ldap.createClient({
             url: process.env.LDAP_URL
         });
@@ -52,6 +63,50 @@ export class AuthService {
         });
     }
 
+    private handleSearch(req: any) {
+        let ldapClient = ldap.createClient({
+            url: process.env.LDAP_URL,
+            bindDN: `${req.username}@isim.intra`,
+            bindCredentials: req.password
+        })
+
+        const opts: ldap.SearchOptions = {
+            filter: `(|(cn=${req.username})(dn=${req.username}))`,
+            scope: 'sub',
+            attributes: ["cn", 'department', 'departmentNumber', 'mail']
+        };
+
+        return new Promise((resolve) => {
+            ldapClient.search(' OU=Etudiants, OU=Comptes,DC=isim, DC=intra', opts, (err, res) => {
+                if (err) {
+                    return resolve({
+                        user: null,
+                        msg: `error on connection : ${err}`
+                    })
+                } else {
+                    res.on('searchEntry', function (entry) {
+                        return resolve({
+                            user: entry.object,
+                            msg: "ok"
+                        })
+                    });
+                    res.on('error', function (err) {
+                        return resolve({
+                            user: null,
+                            msg: `error on search : ${err}`
+                        })
+                    });
+                    res.on('end', function (result) {
+                        return resolve({
+                            user: null,
+                            msg: `function ended : ${result}`
+                        })
+                    });
+                }
+            })
+        })
+    }
+
 
 
     // Login function
@@ -63,14 +118,17 @@ export class AuthService {
         };
     }
 
-    public async loginUser(user: any){
+    public async loginUser(user: any) {
         const payload = {
-            username: user.username
+            username: user.username,
+            mail: user.mail,
+            department: user.department,
+            departmentNumber: user.departmentNumber
         };
         return {
             access_token: this.jwtService.sign(payload)
         };
     }
 
-    
+
 }
